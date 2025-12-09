ba5b3beddd23f81da6871dd9f171aa95dee08c81
wonszux <fabian.kanczurski@gmail.com>
2025-12-09 16:31:03 +0100

    table

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 "use client";
 
+const squares = Array.from({ length: 9 }, (_, i) => i);
+
+const table = squares.map((i) => (
+  <div
+    onClick={() => console.log(i)}
+    key={i}
+    className="h-50 w-50 bg-white"
+  ></div>
+));
+
 export default function Home() {
-  <div>tictactoe</div>;
+  return (
+    <div className="flex flex-row min-h-screen justify-center items-center">
+      <div className="grid grid-cols-3 gap-3">{table}</div>
+    </div>
+  );
 }
